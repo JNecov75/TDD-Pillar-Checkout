fb63d08834788a4d714d902e86ea381b0c08592e
Jordan Necovski <JNecov75@gmail.com>
2019-04-11 23:29:08 -0400

    initial projects

---
+++ PointOfSale.Tests/CartTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PointOfSale.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void WhenAnItemIsScannedItAddsToTheCart()
+        {
+            Cart cart = new Cart();
+            Assert.Fail();
+        }
+    }
+}

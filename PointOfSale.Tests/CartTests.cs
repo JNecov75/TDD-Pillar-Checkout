0b159a87037ea06a2fe738b9ead40a55f1c2fce7
Jordan Necovski <JNecov75@gmail.com>
2019-04-14 15:07:49 -0400

    refactored to create a TestInitialize method to always instantiate the inventory, cuts out code in each test

--- PointOfSale.Tests/CartTests.cs
+++ PointOfSale.Tests/CartTests.cs
@@ -13,11 +13,14 @@ namespace PointOfSale.Tests
             _shop = new Shop();
         }
 
+
+        [TestInitialize()]
+        public void Startup() {
+            _shop.InitializeInventory();
+        }
         [TestMethod]
         public void WhenAnItemIsAddedToInventoryItStoresTheName()
         {
-            _shop.AddToInventory(new Item("soup"));
-            _shop.AddToInventory(new Item("ground beef"));
             Assert.AreEqual("soup", _shop.Inventory[0].Name);
             Assert.AreEqual("ground beef", _shop.Inventory[1].Name);
         }
@@ -25,8 +28,6 @@ namespace PointOfSale.Tests
         [TestMethod]
         public void WhenAnItemIsAddedToInventoryItStoresThePrice()
         {
-            _shop.AddToInventory(new Item("", 1.89));
-            _shop.AddToInventory(new Item("", 5.99));
             Assert.AreEqual(1.89, _shop.Inventory[0].Price);
             Assert.AreEqual(5.99, _shop.Inventory[1].Price);
         }
@@ -34,14 +35,12 @@ namespace PointOfSale.Tests
         [TestMethod]
         public void InventoryCanBeInitializedWithASetOfProducts()
         {
-            _shop.InitializeInventory();
             Assert.AreEqual(2, _shop.Inventory.Count);
         }
 
         [TestMethod]
         public void WhenAnItemIsScannedAddItToTheCart()
         {
-            _shop.InitializeInventory();
             _shop.ScanItem("soup");
             Assert.AreEqual("soup", _shop.Cart[0].Name);
             Assert.AreEqual(1.89, _shop.Cart[0].Price);
@@ -51,7 +50,6 @@ namespace PointOfSale.Tests
         [TestMethod]
         public void WhenAnItemIsAddedToCartItIncrementsQuantity()
         {
-            _shop.InitializeInventory();
             _shop.ScanItem("soup");
             Assert.AreEqual(1, _shop.Cart[0].Quantity);
             _shop.ScanItem("soup");
@@ -61,7 +59,6 @@ namespace PointOfSale.Tests
         [TestMethod]
         public void WhenAnExistingItemIsAddedToCartItUpdatesItem()
         {
-            _shop.InitializeInventory();
             _shop.ScanItem("soup");
             _shop.ScanItem("soup");
             Assert.AreEqual(1, _shop.Cart.Count);
